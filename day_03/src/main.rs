04f2da774074af0f6be26314840a553e5d16324a
Thamer Simon (SFM KAI SDM) <simon.thamer@k-ai.at>
2025-12-03 13:48:59 +0100

    day_03 part_02

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use itertools::Itertools;
@@ -28,25 +27,14 @@ fn main() -> Result<()> {
         input.push(line.chars().map(|c| c.to_digit(10).unwrap() as u64).collect::<Vec<u64>>());
     }
 
-    let part1 = input.par_iter().map(|line| {
-        line.iter().combinations(2).max().unwrap()
-    }).map(|number| {
-        number.iter().rev().enumerate().fold(0, |acc, (idx, value)| {
-            acc + *value * 10u64.pow(idx as u32)
-        })
-    }).sum::<u64>();
-
-    println!("Part 1: {}", part1);
-
-    let part2 = input.par_iter().enumerate().map(|(idx, line)| {
-        // line.iter().combinations(12).max().unwrap()
+    let part1 = input.iter().enumerate().map(|(idx, line)| {
         let mut outvec: Vec<u64> = Vec::new();
-        for current_digit in 11..=0 {
-            let mut last_index = 0;
-            line.iter().rev().enumerate().fold_while(0u64, |acc, (idx, nmbr)| {
-                if idx <= line.len() - current_digit {
-                    if *nmbr > acc && last_index <= idx {
-                        last_index = idx;
+        let mut last_index: isize = -1;
+        for current_digit in (0..=1).rev() {
+            outvec.push(line.iter().enumerate().fold_while(0u64, |acc, (idx, nmbr)| {
+                if idx < line.len() - current_digit {
+                    if *nmbr > acc && last_index < idx as isize {
+                        last_index = idx as isize;
                         if *nmbr == 9 {
                             return  Done(*nmbr);
                         }
@@ -55,14 +43,42 @@ fn main() -> Result<()> {
                     return Continue(acc);
                 }
                 return Done(acc);
-            });
+            }).into_inner());
         }
-        line.iter()
+        outvec
     }).map(|number| {
-        number.rev().enumerate().fold(0, |acc, (idx, value)| {
+        number.iter().rev().enumerate().fold(0, |acc, (idx, value)| {
             acc + *value * 10u64.pow(idx as u32)
         })
-    }).inspect(|x| println!("Finished {x}")).sum::<u64>();
+    }).sum::<u64>();
+
+    println!("Part 1: {}", part1);
+
+
+    let part2 = input.iter().enumerate().map(|(idx, line)| {
+        let mut outvec: Vec<u64> = Vec::new();
+        let mut last_index: isize = -1;
+        for current_digit in (0..=11).rev() {
+            outvec.push(line.iter().enumerate().fold_while(0u64, |acc, (idx, nmbr)| {
+                if idx < line.len() - current_digit {
+                    if *nmbr > acc && last_index < idx as isize {
+                        last_index = idx as isize;
+                        if *nmbr == 9 {
+                            return  Done(*nmbr);
+                        }
+                        return Continue(*nmbr)
+                    }
+                    return Continue(acc);
+                }
+                return Done(acc);
+            }).into_inner());
+        }
+        outvec
+    }).map(|number| {
+        number.iter().rev().enumerate().fold(0, |acc, (idx, value)| {
+            acc + *value * 10u64.pow(idx as u32)
+        })
+    }).sum::<u64>();
 
     println!("Part 2: {}", part2);
     Ok(())
