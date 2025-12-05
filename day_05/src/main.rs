155d09908fdc002217c1d93c76c38adb66e15e4c
Thamer Simon (SFM KAI SDM) <simon.thamer@k-ai.at>
2025-12-05 07:25:22 +0100

    day_05 part_02

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,6 +1,5 @@
 
 use clap::{arg, Parser};
-use itertools::Itertools;
 use std::fs;
 use std::ops::ControlFlow;
 use std::ops::ControlFlow::Continue;
@@ -53,8 +52,34 @@ fn main() {
         false
     }).count();
 
+    let mut ranges_sorted= good_food_ranges;
+    ranges_sorted.sort_by_key(|i| *i.start());
+
+    let mut merged = vec![];
+
+    let mut current = ranges_sorted[0].clone();
+
+    // Go through sorted ist and always compare start next start positions with the next ending.
+    // Ranges overlapping multiple times will be caught in the next iteration.
+    for next in ranges_sorted.into_iter().skip(1) {
+        // +1 since inclusive
+        if *next.start() <= *current.end() + 1 {
+            // Overlapping - merge them
+            current = *current.start()..=(*current.end().max(next.end()));
+        } else {
+            // No overlap - push current and start new range
+            merged.push(current);
+            current = next;
+        }
+    }
+    merged.push(current);
+
+    let part2 = merged.iter().map(|c_iter| {
+        c_iter.end() - c_iter.start() + 1
+    }).sum::<u64>();
+
     println!("Part 1: {}", part1);
-    println!("Part 2: {}", 0);
+    println!("Part 2: {}", part2);
 
     let calc_duration = calc_time.elapsed();
 
