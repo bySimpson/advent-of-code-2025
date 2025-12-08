7895fba14544a64b0adbe813a6131cd5e0dc06fa
Simon <thamersimon@gmail.com>
2025-12-08 15:21:44 +0100

    day_08 part_02

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -1,11 +1,11 @@
-use std::collections::{HashMap, HashSet};
-use itertools::Itertools;
 use clap::{Parser, arg};
+use itertools::Itertools;
+use ordered_float::OrderedFloat;
+use std::collections::HashMap;
 use std::fs;
-use std::iter::Map;
+use std::ops::ControlFlow;
 use std::string::String;
 use std::time::Instant;
-use ordered_float::OrderedFloat;
 
 #[derive(Parser, Clone)]
 #[command(author, version, about, long_about = None)]
@@ -21,20 +21,23 @@ fn euclidian_distance(a: &(u64, u64, u64), b: &(u64, u64, u64)) -> f64 {
     let dy = a.1 as f64 - b.1 as f64;
     let dz = a.2 as f64 - b.2 as f64;
 
-    (dx*dx + dy*dy + dz*dz).sqrt()
+    (dx * dx + dy * dy + dz * dz).sqrt()
 }
 
 fn part1(input: &mut [(u64, u64, u64)], connections: usize) -> u64 {
-    let mut out = 0;
     let mut cluster_amounts: Vec<u64> = Vec::new();
     // coords - cluster number
     let mut connected_boxes: HashMap<(u64, u64, u64), u64> = HashMap::new();
 
-    input.iter().combinations(2).sorted_by_key(|(combinations)| {
-        let p1 = combinations[0];
-        let p2 = combinations[1];
-        OrderedFloat(euclidian_distance(p1, p2))
-    }).get(0..connections)
+    input
+        .iter()
+        .combinations(2)
+        .sorted_by_key(|combinations| {
+            let p1 = combinations[0];
+            let p2 = combinations[1];
+            OrderedFloat(euclidian_distance(p1, p2))
+        })
+        .get(0..connections)
         .for_each(|combinations| {
             let p1 = combinations[0];
             let p2 = combinations[1];
@@ -49,9 +52,12 @@ fn part1(input: &mut [(u64, u64, u64)], connections: usize) -> u64 {
                 let p1_index = connected_boxes[p1];
                 let p2_index = connected_boxes[p2];
                 if p1_index != p2_index {
-                    connected_boxes.iter_mut().filter(|(_, group)| **group == p2_index).for_each(|(_, group)| {
-                        *group = p1_index;
-                    });
+                    connected_boxes
+                        .iter_mut()
+                        .filter(|(_, group)| **group == p2_index)
+                        .for_each(|(_, group)| {
+                            *group = p1_index;
+                        });
                     cluster_amounts[p1_index as usize] += cluster_amounts[p2_index as usize];
                     cluster_amounts[p2_index as usize] = 1;
                 }
@@ -67,12 +73,71 @@ fn part1(input: &mut [(u64, u64, u64)], connections: usize) -> u64 {
                 cluster_amounts[index as usize] += 1;
             }
         });
-    cluster_amounts.iter().sorted().rev().get(0..3).fold(1, |acc, x| acc * x)
+    cluster_amounts
+        .iter()
+        .sorted()
+        .rev()
+        .get(0..3)
+        .product::<u64>()
 }
 
 fn part2(input: &mut [(u64, u64, u64)]) -> u64 {
-    let mut out = 0;
-    out
+    let amount_boxes = input.len();
+    let mut cluster_amounts: Vec<u64> = Vec::new();
+    // coords - cluster number
+    let mut connected_boxes: HashMap<(u64, u64, u64), u64> = HashMap::new();
+
+    let output = input
+        .iter()
+        .combinations(2)
+        .sorted_by_key(|combinations| {
+            let p1 = combinations[0];
+            let p2 = combinations[1];
+            OrderedFloat(euclidian_distance(p1, p2))
+        })
+        .try_for_each(|combinations| {
+            let p1 = combinations[0];
+            let p2 = combinations[1];
+            if !connected_boxes.contains_key(p1) && !connected_boxes.contains_key(p2) {
+                // both not found, new cluster group
+                cluster_amounts.push(2);
+                let index = cluster_amounts.len() as u64 - 1;
+                connected_boxes.insert(*p1, index);
+                connected_boxes.insert(*p2, index);
+            } else if connected_boxes.contains_key(p1) && connected_boxes.contains_key(p2) {
+                // both exist, connect clusters
+                let p1_index = connected_boxes[p1];
+                let p2_index = connected_boxes[p2];
+                if p1_index != p2_index {
+                    connected_boxes
+                        .iter_mut()
+                        .filter(|(_, group)| **group == p2_index)
+                        .for_each(|(_, group)| {
+                            *group = p1_index;
+                        });
+                    cluster_amounts[p1_index as usize] += cluster_amounts[p2_index as usize];
+                    cluster_amounts[p2_index as usize] = 1;
+                    if cluster_amounts[p1_index as usize] == amount_boxes as u64 {
+                        return ControlFlow::Break(p1.0 * p2.0);
+                    }
+                }
+            } else {
+                // one already has cluster group, connect to it
+                let index = if connected_boxes.contains_key(p1) {
+                    connected_boxes[p1]
+                } else {
+                    connected_boxes[p2]
+                };
+                connected_boxes.insert(*p1, index);
+                connected_boxes.insert(*p2, index);
+                cluster_amounts[index as usize] += 1;
+                if cluster_amounts[index as usize] == amount_boxes as u64 {
+                    return ControlFlow::Break(p1.0 * p2.0);
+                }
+            }
+            ControlFlow::Continue(())
+        });
+    output.break_value().unwrap()
 }
 
 fn main() {
