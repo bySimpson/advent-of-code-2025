600f6ff2a23a11e8e6a562c3a4248dd2e2cf758f
Simon <thamersimon@gmail.com>
2025-12-10 15:09:23 +0100

    day_10 part_2 example finishing

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -40,8 +40,37 @@ fn part1(button_presses: &[u64], solution: u64) -> u64 {
     iteration
 }
 
-fn part2(input: &mut [(u64, u64)]) -> u64 {
-    0
+fn part2(button_presses: &[u64], required_joltages: &[u64]) -> u64 {
+    let mut c_operation: HashSet<Vec<u64>> = HashSet::new();
+    c_operation.insert(vec![0; required_joltages.len()]); // start with a blank field
+    let mut iteration = 0;
+    'outer: loop {
+        iteration += 1;
+        let mut next: HashSet<Vec<u64>> = HashSet::new();
+
+        for operation in c_operation {
+            for c_button_press in button_presses.iter() {
+                let mut current= Vec::new();
+                for (i, c_joltage_level) in operation.iter().enumerate() {
+                    let change = if c_button_press & (1 << i) > 0 {1} else {0};
+                    let new = c_joltage_level + change;
+                    if new > required_joltages[i] {
+                        // skip, not viable
+                        break;
+                    }
+                    current.push(new);
+                }
+                if current == required_joltages {
+                    break 'outer;
+                } else {
+                    next.insert(current);
+                }
+            }
+        }
+
+        c_operation = next;
+    }
+    iteration
 }
 
 fn main() {
@@ -52,6 +81,7 @@ fn main() {
     let read = fs::read_to_string(args.path).unwrap();
     let mut button_presses: Vec<Vec<u64>> = Vec::new();
     let mut solutions: Vec<u64> = Vec::new();
+    let mut required_joltage: Vec<Vec<u64>> = Vec::new();
     read.lines()
         .for_each(|line| {
             let mut c_presses: Vec<u64> = Vec::new();
@@ -70,6 +100,8 @@ fn main() {
                             c_press += 1 << n;
                         });
                         c_presses.push(c_press);
+                    } else {
+                        required_joltage.push(s.replace("{", "").replace("}", "").split(",").map(|c| c.parse::<u64>().unwrap()).collect::<Vec<u64>>())
                     }
                 }
             });
@@ -86,7 +118,9 @@ fn main() {
     let par1_duration = part1_time.elapsed();
 
     let part2_time = Instant::now();
-    let part2 = 0;
+    let part2 = required_joltage.iter().enumerate().map(|(i, required_joltage)| {
+        part2(&*button_presses[i], required_joltage)
+    }).inspect(|c| println!("{c}")).sum::<u64>();
 
     let part2_duration = part2_time.elapsed();
     let calc_duration = calc_time.elapsed();
